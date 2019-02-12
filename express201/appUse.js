664df1cabf1113d8b3534940e7ac42e9711aaad2
Jack Gamboa <jgamboa929@gmail.com>
2019-02-12 16:59:23 -0600

    Express cont

--- express201/appUse.js
+++ express201/appUse.js
@@ -1,9 +1,21 @@
+// Middleware function is any function that has access to the req, res, next object.
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Home page</h1>");
+function validateUser(req, res, next) {
+  // res object property 'locals'.
+  res.locals.validated = true;
+  res.next();
+}
+
+app.use(validateUser); // This function is going to run everytime an HTTP request is made
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Main Page</h1>");
+  console.log("VALIDATED RAN!");
+  next();
 });
+
 app.listen(3000, () => {
   console.log("Server started!");
 });
