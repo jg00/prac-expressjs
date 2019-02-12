e183ace1cbaabd46a7ce685f74922877959c75bc
Jack Gamboa <jgamboa929@gmail.com>
2019-02-12 16:34:43 -0600

    Express

--- express201/appUse.js
+++ express201/appUse.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("<h1>Home page</h1>");
+});
 app.listen(3000, () => {
   console.log("Server started!");
 });
