e6a76f155e283c20922876d95889d5e164c3db4b
Jack Gamboa <jgamboa929@gmail.com>
2019-02-13 13:47:00 -0600

    Middleware cont - appUse

--- express201/appUse.js
+++ express201/appUse.js
@@ -3,17 +3,30 @@ const express = require("express");
 const app = express();
 
 function validateUser(req, res, next) {
-  // res object property 'locals'.
+  // res object has a property called 'locals'.  res.locals is an object
+  // that contains response local variables scoped to the request
   res.locals.validated = true;
-  res.next();
+  console.log("VALIDATED RAN!");
+  next();
 }
 
 app.use(validateUser); // This function is going to run everytime an HTTP request is made
 
+/*
+  notes:
+  app.use(validateUser); // Will run validateUser on all routes, all methods
+  app.use("/admin", validateUser); // Will run validateUser on /admin route, all methods
+  app.get("/", validateUser); // Will run validateUser on / route, only on GET method
+*/
+
 app.get("/", (req, res, next) => {
   res.send("<h1>Main Page</h1>");
-  console.log("VALIDATED RAN!");
-  next();
+  console.log(res.locals.validated);
+});
+
+app.get("/admin", (req, res, next) => {
+  res.send("<h1>Admin Page</h1>");
+  console.log(res.locals.validated);
 });
 
 app.listen(3000, () => {
