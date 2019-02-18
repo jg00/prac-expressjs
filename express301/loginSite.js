1a06a99e27a5667ad57d3e2baf7cd3014534a14e
Jack Gamboa <jgamboa929@gmail.com>
2019-02-18 11:01:27 -0600

    Login and set cookie.

--- express301/loginSite.js
+++ express301/loginSite.js
@@ -2,11 +2,13 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
 
 app.use(helmet());
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -29,17 +31,27 @@ app.post("/process_login", (req, res, next) => {
   // session data is stored on server and browser is given essentially a key for that data. (express-sessions are not included in express by default).
   // send to welcome page
 
-  if (password === "password") {
+  if (password === "s") {
     // .cookie('name of cookie', 'value')
     res.cookie("username", username);
     res.redirect("/welcome");
+  } else {
+    res.redirect("/login?msg=fail");
   }
 
-  //   res.json(req.body);
+  // res.json(req.body);
 });
 
 app.get("/welcome", (req, res, next) => {
-  res.send("Welcome!");
+  res.render("welcome", {
+    username: req.cookies.username
+  });
+});
+
+app.get("/logout", (req, res, nex) => {
+  // .clearCookie('name of cookie')
+  res.clearCookie("username");
+  res.redirect("/login");
 });
 
 app.listen(3000, () => console.log("Server started!"));
