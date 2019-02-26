601272fbe9e60f7fac7c63a0ac5f6bff3221fe2a
Jack Gamboa <jgamboa929@gmail.com>
2019-02-26 10:49:06 -0600

    MovieFanSite - index page and movie details page.

--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -9,6 +9,11 @@ const apiBaseUrl = "http://api.themoviedb.org/3";
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
 const imageBaseUrl = "http://image.tmdb.org/t/p/w300";
 
+router.use((req, res, next) => {
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next();
+});
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
   // console.log(nowPlayingUrl);
@@ -33,11 +38,30 @@ router.get("/", function(req, res, next) {
 
     // You could also run against response.body
     // JSON.parse will convert the string to a JSON object
-    const parsedData = JSON.parse(movieData);
-    console.log(parsedData);
+    const parsedData = JSON.parse(movieData); // movieData is an object and we are interested in the results array of movie objects
+    // console.log(parsedData);
+
+    // res.json(parsedData);
+    res.render("index", { parsedData: parsedData.results });
   });
 
-  res.render("index", {});
+  // res.render("index", {});
+});
+
+router.get("/movie/:id", (req, res, next) => {
+  // res.json(req.params.id); // check only
+  const movieId = req.params.id;
+  const thisMovieURL = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`;
+
+  // res.send(thisMovieURL); // check
+
+  // Now need to issue a new HTTP request to the Movie API
+  // Here we are acting as client to Movie Database API eventhough we are a server)
+  // http://api.themoviedb.org/3/movie/{movie_id} > to GET movie details
+  request.get(thisMovieURL, (error, response, movieData) => {
+    const parsedData = JSON.parse(movieData); // movieData returned is an object
+    res.render("single-movie", { parsedData });
+  });
 });
 
 module.exports = router;
