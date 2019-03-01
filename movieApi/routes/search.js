67662f1b0cdcb87f397179e62a5f58b8a46c46f1
Jack Gamboa <jgamboa929@gmail.com>
2019-03-01 16:36:17 -0600

    Added search routes for custom API.

--- movieApi/routes/search.js
+++ movieApi/routes/search.js
@@ -1,10 +1,46 @@
 const express = require("express");
 const router = express.Router();
 
+const movies = require("../data/movies");
+const people = require("../data/people");
+
+function queryRequired(req, res, next) {
+  const searchTerm = req.query.query;
+  if (!searchTerm) {
+    res.json({ msg: "Query is required." });
+  } else {
+    next();
+  }
+}
+
+// This middleware will be used by ALL routes in THIS router
+router.use(queryRequired);
+
 // GET /search/
 router.get("/", (req, res, next) => {
   //   console.log(req.headers);
   res.send("Search route check");
+});
+
+// GET /search/movie
+router.get("/movie", (req, res, next) => {
+  const searchTerm = req.query.query;
+  const results = movies.filter(movie => {
+    let found =
+      movie.overview.includes(searchTerm) || movie.title.includes(searchTerm);
+    return found;
+  });
+  res.json({ results });
+});
+
+// GET /search/person
+router.get("/person", (req, res, next) => {
+  const searchTerm = req.query.query;
+  const results = people.filter(person => {
+    let found = person.name.includes(searchTerm);
+    return found;
+  });
+  res.json({ results });
 });
 
 module.exports = router;
